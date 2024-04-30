b4c50800256ee932ad9a05a18853d0a68f1ed29c
Swan <swan@Swanui-MacBookAir.local>
2024-04-30 15:34:57 +0900

    init project

---
+++ src/main/java/com/swan/project24/Project24Application.java
@@ -0,0 +1,16 @@
+package com.swan.project24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * cicd연습
+ */
+@SpringBootApplication
+public class Project24Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Project24Application.class, args);
+	}
+
+}
